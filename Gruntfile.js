2bda5ecc8b0eb63cb15c310f29ef11a616bcd465
hmabesa <hbmmitzer-git@yahoo.com>
2014-02-15 18:15:33 -0500

    set up grunt build to generate node & amd options

---
+++ Gruntfile.js
@@ -0,0 +1,85 @@
+module.exports = function(grunt) {
+
+    var config = {
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        clean: {
+            'dest': 'js/*'
+        },
+
+        copy: {
+
+            orig: {
+                src: 'src/DateTime.js',
+                dest: 'js/DateTime.js'
+            },
+
+            node: {
+                options: {
+                    process: function(content, path) {
+                        var banner = 'module.exports = function() {\n\n',
+                            footer = '\n\nreturn DateTime;\n\n};';
+
+                        var ret = banner + content + footer;
+                        return ret;
+                    }
+                },
+                src: 'src/DateTime.js',
+                dest: 'js/DateTime.node.js'
+            },
+
+            amd: {
+                options: {
+                    process: function(content, path) {
+                        var banner = 'define("DateTime", [], function() {\n\n',
+                            footer = '\n\nreturn DateTime;\n\n});';
+
+                        var ret = banner + content + footer;
+                        return ret;
+                    }
+                },
+                src: 'src/DateTime.js',
+                dest: 'js/DateTime.amd.js'
+            }
+
+        },
+
+        jshint: {
+
+            build: {
+
+                options: grunt.file.readJSON('.jshintrc'),
+                expand: true,
+                src: ['src/**.js']
+
+            }
+
+        },
+
+        uglify: {
+
+            original: {
+
+                options: {
+                    mangle: true
+                },
+                expand: true,
+                files: {
+                    'js/DateTime.min.js': 'src/DateTime.js'
+                }
+
+            }
+
+        }
+
+    };
+
+    // load npm's
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    grunt.registerTask('default', ['clean', 'jshint', 'copy', 'uglify']);
+
+    grunt.initConfig(config);
+
+};
